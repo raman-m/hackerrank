57f2f1e6642e5104f2d4757b6a6f38c9cb429157
Raman Maksimchuk <dotnet044@gmail.com>
2023-01-25 19:26:24 +0300

    Complete "Sample Test cases" for 'Solve Me First' challenge

--- dotnet/tests/Algorithms/Warmup/SolveMeFirst/TestCases.cs
+++ dotnet/tests/Algorithms/Warmup/SolveMeFirst/TestCases.cs
@@ -1,73 +1,26 @@
 ﻿using RamanM.HackerRank.Algorithms.Warmup.SolveMeFirst;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace RamanM.HackerRank.Tests.Algorithms.Warmup.SolveMeFirst
+namespace RamanM.HackerRank.Tests.Algorithms.Warmup.SolveMeFirst;
+
+public class TestCases
 {
-    public class TestCases
+    [Theory]
+    [InlineData("00")]
+    [InlineData("01")]
+    public void Solution_SolveMeFirst_SampleTestCaseXX(string code)
     {
-        [Fact]
-        public void Solution_SolveMeFirst_SampleTestCase0()
-        {
-            // Arrange
-            var basePath = Path.Combine(Environment.CurrentDirectory, "Algorithms\\Warmup\\SolveMeFirst\\");
-            var @case = new TestCase(basePath);
-            @case.InputPath = "input\\input00.txt";
-            @case.OutputPath = "output\\output00.txt";
+        // Arrange
+        var basePath = Path.Combine(Environment.CurrentDirectory, "Algorithms\\Warmup\\SolveMeFirst\\");
+        var test = new TestCase(basePath);
+        test.InputPath = $"input\\input{code}.txt";
+        test.OutputPath = $"output\\output{code}.txt";
+        var expected = File.ReadAllText(test.OutputPath);
 
-            var expected = File.ReadAllText(@case.OutputPath);
+        // Act
+        test.ActInConsole(() => Solution.Main(Array.Empty<string>()));
 
-            var path = Path.GetDirectoryName(@case.OutputPath);
-            var fileName = Path.GetFileNameWithoutExtension(@case.OutputPath);
-            var timeStamp = DateTime.Now.TimeOfDay.ToString("hhmmss");
-            var file = $"{fileName}_actual_{timeStamp}.txt";
-            var actualFile = Path.Combine(path, file);
-
-            try
-            {
-                // Attempt to open output file.
-                using (var writer = new StreamWriter(actualFile))
-                {
-                    using (var reader = new StreamReader(@case.InputPath))
-                    {
-                        Console.SetOut(writer); // Redirect standard output from the console to the output file.                        
-                        Console.SetIn(reader); // Redirect standard input from the console to the input file.
-
-                        // Act
-                        Solution.Main(Array.Empty<string>());
-                    }
-                }
-            }
-            catch (IOException e)
-            {
-                TextWriter errorWriter = Console.Error;
-                errorWriter.WriteLine(e.Message);
-                throw e;
-            }
-
-            try
-            {
-                // Assert
-                var actual = File.ReadAllText(actualFile).Trim(new char[] { '\t', '\n', '\r' });
-                Assert.Equal(expected, actual);
-            }
-            finally
-            {
-                TearDown(nameof(Solution_SolveMeFirst_SampleTestCase0));
-            }
-        }
-
-        private void TearDown(string testCaseName)
-        {
-            // Recover the standard output stream so that a completion message can be displayed.
-            var standardOutput = new StreamWriter(Console.OpenStandardOutput());
-            standardOutput.AutoFlush = true;
-            Console.SetOut(standardOutput);
-            Console.WriteLine($"{nameof(TestCases)} has completed the running of the {testCaseName} test case.");
-            Console.WriteLine($"{nameof(TearDown)} has succeeded.");
-        }
+        // Assert
+        var actual = File.ReadAllText(test.TestResultPath).Trim(new char[] { '\t', '\n', '\r' });
+        Assert.Equal(expected, actual);
     }
 }
--- dotnet/tests/TestCase.cs
+++ dotnet/tests/TestCase.cs
@@ -1,70 +1,106 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
-using System.IO.Compression;
-using Newtonsoft.Json.Linq;
+﻿namespace RamanM.HackerRank.Tests;
 
-namespace RamanM.HackerRank.Tests
+public class TestCase
 {
-    public class TestCase
+    private const string testCaseDir = "testcases";
+    private readonly string basePath = string.Empty;
+
+    private TestCase() { }
+
+    public TestCase(string basePath)
     {
-        private const string testCaseDir = "testcases";
-        private readonly string basePath = string.Empty;
+        if (!Directory.Exists(basePath))
+            throw new DirectoryNotFoundException(basePath);
 
-        private TestCase() { }
+        var dir = Path.Combine(basePath, testCaseDir);
+        if (!Directory.Exists(dir))
+            throw new DirectoryNotFoundException(dir);
 
-        public TestCase(string basePath)
+        this.basePath = basePath;
+    }
+
+    private string _inputFile;
+    public string InputPath
+    {
+        get => Path.Combine(basePath, testCaseDir, _inputFile);
+        set
         {
-            if (!Directory.Exists(basePath))
-                throw new DirectoryNotFoundException(basePath);
-
-            var dir = Path.Combine(basePath, testCaseDir);
-            if (!Directory.Exists(dir))
-                throw new DirectoryNotFoundException(dir);
-
-            this.basePath = basePath;
+            CheckFile(value);
+            _inputFile = value;
         }
+    }
 
-        private string _inputFile;
-        public string InputPath
+    private string _outputFile;
+    public string OutputPath
+    {
+        get => Path.Combine(basePath, testCaseDir, _outputFile);
+        set
         {
-            get => Path.Combine(basePath, testCaseDir, _inputFile);
-            set
+            CheckFile(value);
+            _outputFile = value;
+        }
+    }
+
+    private void CheckFile(string fileName)
+    {
+        var file = Path.Combine(basePath, testCaseDir, fileName);
+        if (!File.Exists(file))
+            throw new FileNotFoundException(file);
+    }
+
+    private string _testResultPath;
+    public string TestResultPath
+    {
+        get
+        {
+            if (string.IsNullOrEmpty(_testResultPath))
             {
-                CheckFile(value);
-                _inputFile = value;
+                var testcasesDir = Directory.GetParent(OutputPath).Parent;
+                var fileName = Path.GetFileNameWithoutExtension(OutputPath);
+                var timeStamp = DateTime.Now.TimeOfDay.ToString().Replace(":", string.Empty);
+                var file = $"{fileName}_actual_{timeStamp}.txt";
+                _testResultPath = Path.Combine(testcasesDir.FullName, file);
             }
+            return _testResultPath;
         }
+        set => _testResultPath = value;
+    }
 
-        private string _outputFile;
-        public string OutputPath
+    public FileStream Input()
+    {
+        return File.OpenRead(InputPath);
+    }
+
+    public FileStream Output()
+    {
+        return File.OpenRead(OutputPath);
+    }
+
+    public void Act(Action action) => action?.Invoke();
+
+
+    public void ActInConsole(Action action)
+    {
+        try
         {
-            get => Path.Combine(basePath, testCaseDir, _outputFile);
-            set
-            {
-                CheckFile(value);
-                _outputFile = value;
-            }
+            using var writer = new StreamWriter(TestResultPath); // Attempt to open output file.
+            using var reader = new StreamReader(InputPath);
+            
+            Console.SetOut(writer); // Redirect standard output from the console to the output file.                        
+            Console.SetIn(reader); // Redirect standard input from the console to the input file.
+
+            action?.Invoke(); // Act
         }
-
-        private void CheckFile(string fileName)
+        catch (IOException e)
         {
-            var file = Path.Combine(basePath, testCaseDir, fileName);
-            if (!File.Exists(file))
-                throw new FileNotFoundException(file);
+            Console.Error.WriteLine(e.Message);
+            throw e;
         }
-
-        public Stream Input()
+        finally
         {
-            return File.OpenRead(InputPath);
-        }
-
-        public Stream Output()
-        {
-            return File.OpenRead(OutputPath);
+            var standardOutput = new StreamWriter(Console.OpenStandardOutput());
+            standardOutput.AutoFlush = true;
+            Console.SetOut(standardOutput); // Recover the standard output stream 
         }
     }
 }
