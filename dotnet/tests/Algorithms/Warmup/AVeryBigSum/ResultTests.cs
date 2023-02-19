a43fbed6d38a3f816297c88f199a0f851fcee99c
Raman Maksimchuk <dotnet044@gmail.com>
2023-02-19 14:46:03 +0300

    Algorithms > Warmup > 'A Very Big Sum' challenge

---
+++ dotnet/tests/Algorithms/Warmup/AVeryBigSum/ResultTests.cs
@@ -0,0 +1,21 @@
+﻿using RamanM.HackerRank.Algorithms.Warmup.AVeryBigSum;
+
+namespace RamanM.HackerRank.Tests.Algorithms.Warmup.AVeryBigSum;
+
+public class ResultTests
+{
+    [Fact]
+    public void AVeryBigSum_Sample_ReturnsOutput()
+    {
+        // Arrange
+        const int n = 5;
+        var ar = new List<long>(n) { 1000000001, 1000000002, 1000000003, 1000000004, 1000000005 };
+        long output = 5000000015;
+
+        // Act
+        long actual = Result.aVeryBigSum(ar);
+
+        // Assert
+        Assert.Equal(output, actual);
+    }
+}
---
+++ dotnet/tests/Algorithms/Warmup/AVeryBigSum/SolutionTests.cs
@@ -0,0 +1,25 @@
+﻿using RamanM.HackerRank.Algorithms.Warmup.AVeryBigSum;
+
+namespace RamanM.HackerRank.Tests.Algorithms.Warmup.AVeryBigSum;
+
+[Collection(nameof(SolutionTests))]
+public class SolutionTests : ConsoleTest
+{
+    [Fact]
+    public void Main_Sample_ReturnsOutput()
+    {
+        // Arrange
+        string nl = Environment.NewLine;
+        var stdin = $"5{nl}1000000001 1000000002 1000000003 1000000004 1000000005";
+        var output = "5000000015" + nl;
+        var outputPath = SetupOutput();
+
+        // Act
+        Act(() => Solution.Main(Array.Empty<string>()),
+            stdin);
+
+        // Assert
+        var actual = Actual(outputPath);
+        Assert.Equal(output, actual);
+    }
+}
