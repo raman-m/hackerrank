25de400cbe98b670807a12f9039f24d22478fb4c
Raman Maksimchuk <dotnet044@gmail.com>
2023-02-20 15:01:13 +0300

    Algorithms > Warmup > 'Diagonal Difference' challenge

---
+++ dotnet/tests/Algorithms/Warmup/DiagonalDifference/ResultTests.cs
@@ -0,0 +1,44 @@
+﻿using RamanM.HackerRank.Algorithms.Warmup.DiagonalDifference;
+
+namespace RamanM.HackerRank.Tests.Algorithms.Warmup.DiagonalDifference;
+
+public class ResultTests
+{
+    [Fact]
+    public void DiagonalDifference_Example_ReturnsMatrixModule()
+    {
+        // Arrange
+        var arr = new List<List<int>>
+        {
+            new List<int> { 1, 2, 3 },
+            new List<int> { 4, 5, 6 },
+            new List<int> { 9, 8, 9 },
+        };
+        const int matrixModule = 2;
+
+        // Act
+        var actual = Result.diagonalDifference(arr);
+
+        // Assert
+        Assert.Equal(matrixModule, actual);
+    }
+
+    [Fact]
+    public void DiagonalDifference_Sample_ReturnsMatrixModule()
+    {
+        // Arrange
+        var arr = new List<List<int>>
+        {
+            new List<int> { 11, 2, 4 },
+            new List<int> { 4, 5, 6 },
+            new List<int> { 10, 8, -12 },
+        };
+        const int matrixModule = 15;
+
+        // Act
+        var actual = Result.diagonalDifference(arr);
+
+        // Assert
+        Assert.Equal(matrixModule, actual);
+    }
+}
---
+++ dotnet/tests/Algorithms/Warmup/DiagonalDifference/SolutionTests.cs
@@ -0,0 +1,43 @@
+﻿using RamanM.HackerRank.Algorithms.Warmup.DiagonalDifference;
+
+namespace RamanM.HackerRank.Tests.Algorithms.Warmup.DiagonalDifference;
+
+[Collection(nameof(SolutionTests))]
+public class SolutionTests : ConsoleTest
+{
+    private readonly string nl = Environment.NewLine;
+
+    [Fact]
+    public void Main_Example_ReturnsMatrixModule()
+    {
+        // Arrange
+        var stdin = $"3{nl}1 2 3{nl}4 5 6{nl}9 8 9";
+        var output = "2" + nl;
+        var outputPath = SetupOutput();
+
+        // Act
+        Act(() => Solution.Main(Array.Empty<string>()),
+            stdin);
+
+        // Assert
+        var actual = Actual(outputPath);
+        Assert.Equal(output, actual);
+    }
+
+    [Fact]
+    public void Main_Sample_ReturnsMatrixModule()
+    {
+        // Arrange
+        var stdin = $"3{nl}11 2 4{nl}4 5 6{nl}10 8 -12";
+        var output = "15" + nl;
+        var outputPath = SetupOutput();
+
+        // Act
+        Act(() => Solution.Main(Array.Empty<string>()),
+            stdin);
+
+        // Assert
+        var actual = Actual(outputPath);
+        Assert.Equal(output, actual);
+    }
+}
