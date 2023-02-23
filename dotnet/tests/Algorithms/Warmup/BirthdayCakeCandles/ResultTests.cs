0ab90051da670606d7b156d1da0d3e29b426134d
Raman Maksimchuk <dotnet044@gmail.com>
2023-02-23 13:35:19 +0300

    Algorithms > Warmup > 'Birthday Cake Candles' challenge

---
+++ dotnet/tests/Algorithms/Warmup/BirthdayCakeCandles/ResultTests.cs
@@ -0,0 +1,34 @@
+﻿using RamanM.HackerRank.Algorithms.Warmup.BirthdayCakeCandles;
+
+namespace RamanM.HackerRank.Tests.Algorithms.Warmup.BirthdayCakeCandles;
+
+public class ResultTests
+{
+    [Fact]
+    public void BirthdayCakeCandles_Example_ReturnsTwo()
+    {
+        // Arrange
+        List<int> candles = new List<int> { 4, 4, 1, 3 };
+        const int two = 2;
+
+        // Act
+        int actual = Result.birthdayCakeCandles(candles);
+
+        // Assert
+        Assert.Equal(two, actual);
+    }
+
+    [Fact]
+    public void BirthdayCakeCandles_Sample_ReturnsTwo()
+    {
+        // Arrange
+        List<int> candles = new List<int> { 3, 2, 1, 3 };
+        const int two = 2;
+
+        // Act
+        int actual = Result.birthdayCakeCandles(candles);
+
+        // Assert
+        Assert.Equal(two, actual);
+    }
+}
---
+++ dotnet/tests/Algorithms/Warmup/BirthdayCakeCandles/SolutionTests.cs
@@ -0,0 +1,35 @@
+﻿using RamanM.HackerRank.Algorithms.Warmup.BirthdayCakeCandles;
+
+namespace RamanM.HackerRank.Tests.Algorithms.Warmup.BirthdayCakeCandles;
+
+[Collection(nameof(Console))]
+public class SolutionTests : ConsoleTest
+{
+    [Fact]
+    public void Main_Example_ReturnsTwo()
+    {
+        // Arrange
+        var stdin = $"4{nl}4 4 1 3";
+        var two = "2" + nl;
+
+        // Act
+        var actual = ActOnOutput(() => Solution.Main(Array.Empty<string>()), stdin);
+
+        // Assert
+        Assert.Equal(two, actual);
+    }
+
+    [Fact]
+    public void Main_Sample_ReturnsTwo()
+    {
+        // Arrange
+        var stdin = $"4{nl}3 2 1 3";
+        var two = "2" + nl;
+
+        // Act
+        var actual = ActOnOutput(() => Solution.Main(Array.Empty<string>()), stdin);
+
+        // Assert
+        Assert.Equal(two, actual);
+    }
+}
