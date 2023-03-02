2165625433bc51476e69f32cb35b8d34e09c1e57
Raman Maksimchuk <dotnet044@gmail.com>
2023-03-02 23:10:10 +0300

    Algorithms > Implementation > 'Cats and a Mouse' challenge

---
+++ algorithms/implementation/cats-and-a-mouse/Solution.cs
@@ -0,0 +1,59 @@
+﻿namespace RamanM.HackerRank.Algorithms.Implementation.CatsAndMouse;
+
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+public class Solution
+{
+    const string CatA = "Cat A", CatB = "Cat B", MouseC = "Mouse C";
+
+    // Complete the catAndMouse function below.
+    public static string catAndMouse(int x, int y, int z)
+    {
+        int distanceA = Math.Abs(z - x);
+        int distanceB = Math.Abs(z - y);
+
+        if (distanceA == distanceB)
+            return MouseC;
+        else if (distanceA < distanceB)
+            return CatA;
+        else
+            return CatB;
+    }
+
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int q = Convert.ToInt32(Console.ReadLine());
+
+        for (int qItr = 0; qItr < q; qItr++)
+        {
+            string[] xyz = Console.ReadLine().Split(' ');
+
+            int x = Convert.ToInt32(xyz[0]);
+
+            int y = Convert.ToInt32(xyz[1]);
+
+            int z = Convert.ToInt32(xyz[2]);
+
+            string result = catAndMouse(x, y, z);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
---
+++ dotnet/tests/Algorithms/Implementation/CatsAndMouse/SolutionTests.cs
@@ -0,0 +1,63 @@
+﻿using RamanM.HackerRank.Algorithms.Implementation.CatsAndMouse;
+
+namespace RamanM.HackerRank.Tests.Algorithms.Implementation.CatsAndMouse;
+
+public class SolutionCatAndMouseTests
+{
+    [Fact]
+    public void CatAndMouse_Example_CatBCatchesTheMouse()
+    {
+        // Arrange
+        const int x = 2, y = 5, z = 4;
+
+        // Act
+        string actual = Solution.catAndMouse(x, y, z);
+
+        // Assert
+        Assert.Equal("Cat B", actual);
+    }
+
+    [Theory]
+    [InlineData(1, 2, 3, "Cat B")]
+    [InlineData(1, 3, 2, "Mouse C")]
+    public void CatAndMouse_Sample_ReturnsCatsOrMouse(int x, int y, int z, string expected)
+    {
+        // Arrange, Act
+        string actual = Solution.catAndMouse(x, y, z);
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+}
+
+[Collection(nameof(Console))]
+public class SolutionTests : ConsoleTest
+{
+    [Fact]
+    public void Main_Example_CatBCatchesTheMouse()
+    {
+        // Arrange
+        var stdin = $"1{nl}2 5 4";
+        var catB = "Cat B" + nl;
+
+        // Arrange
+        var actual = ActOnOutput(() => Solution.Main(Array.Empty<string>()), stdin);
+
+        // Assert
+        Assert.Equal(catB, actual);
+    }
+
+    [Fact]
+    public void Main_Sample_ReturnsCatsOrMouse()
+    {
+        // Arrange
+        var stdin = $"2{nl}1 2 3{nl}1 3 2";
+        var nine = $"Cat B{nl}Mouse C{nl}";
+
+        // Act
+        var actual = ActOnOutput(() => Solution.Main(Array.Empty<string>()), stdin);
+
+        // Assert
+        Assert.Equal(nine, actual);
+    }
+}
