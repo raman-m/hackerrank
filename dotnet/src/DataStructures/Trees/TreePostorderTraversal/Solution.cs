fe0e488499203b658e9d078a830f0f1f737bacf6
Raman Maksimchuk <dotnet044@gmail.com>
2023-03-14 21:05:02 +0300

    Data Structures > Trees > 'Tree: Postorder Traversal' challenge

---
+++ dotnet/src/DataStructures/Trees/TreePostorderTraversal/Solution.cs
@@ -0,0 +1,70 @@
+﻿namespace RamanM.HackerRank.DataStructures.Trees.TreePostorderTraversal;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+public class Node
+{
+    public Node left;
+    public Node right;
+    public int data;
+
+    public Node(int data)
+    {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+public class Solution
+{
+    public static void postOrder(Node root)
+    {
+        if (root == null)
+            return;
+
+        postOrder(root.left);
+        postOrder(root.right);
+
+        Console.Write(root.data + " ");
+    }
+
+    public static Node insert(Node root, int data)
+    {
+        if (root == null)
+        {
+            return new Node(data);
+        }
+        else
+        {
+            Node current;
+            if (data <= root.data)
+            {
+                current = insert(root.left, data);
+                root.left = current;
+            }
+            else
+            {
+                current = insert(root.right, data);
+                root.right = current;
+            }
+            return root;
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+        var nodes = Console.ReadLine().TrimEnd().Split(' ').Select(iStr => Convert.ToInt32(iStr));
+
+        Node root = null;
+        foreach (int data in nodes.Take(n))
+        {
+            root = insert(root, data);
+        }
+
+        postOrder(root);
+    }
+}
---
+++ dotnet/tests/DataStructures/Trees/TreePostorderTraversal/SolutionTests.cs
@@ -0,0 +1,50 @@
+﻿using RamanM.HackerRank.DataStructures.Trees.TreePostorderTraversal;
+
+namespace RamanM.HackerRank.Tests.DataStructures.Trees.TreePostorderTraversal;
+
+[Collection(nameof(Console))]
+public class SolutionTests : ConsoleTest
+{
+    [Fact]
+    public void PostOrder_Sample_PrintsTree()
+    {
+        // Arrange
+        var root = new Node(1);
+        var n2 = new Node(2);
+        root.right = n2;
+
+        var n5 = new Node(5);
+        n2.right = n5;
+
+        var n3 = new Node(3);
+        n5.left = n3;
+
+        var n4 = new Node(4);
+        n3.right = n4;
+
+        var n6 = new Node(6);
+        n5.right = n6;
+
+        var printed = "4 3 6 5 2 1 ";
+
+        // Act
+        var actual = Act(() => Solution.postOrder(root));
+
+        // Assert
+        Assert.Equal(printed, actual);
+    }
+
+    [Fact]
+    public void Main_Sample_PrintsTree()
+    {
+        // Arrange
+        var stdin = Encoding.ASCII.GetBytes($"6{nl}1 2 5 3 6 4");
+        var printed = "4 3 6 5 2 1 ";
+
+        // Act
+        var actual = Act(() => Solution.Main(Array.Empty<string>()), stdin);
+
+        // Assert
+        Assert.Equal(printed, actual);
+    }
+}
